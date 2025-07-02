3c631bf054273a1ed3b2dc5c2c6cd9be0c87a092
hama <120517448+hama202019@users.noreply.github.com>
2025-07-02 14:07:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	http.HandleFunc("GET /", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("hello, world!"))
 	})
+	http.HandleFunc("GET /health", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("ok, available!"))
+	})
 	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
